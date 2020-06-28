a90af5ffc3bd3a70f7e9a57fa6e624ebdd902446
rustamrv <rystmarv@gmail.com>
2020-06-28 11:42:04 +0300

    add interface vertices and edges

--- base.py
+++ base.py
@@ -1,33 +1,43 @@
 
+
+class Vertex:
+
+    def __init__(self, name):
+        self.name = name
+        self.__edges = dict()
+
+    def __str__(self):
+        return self.name
+
+    def add_edge(self, vertex, weight=1):
+        self.__edges[vertex] = weight
+
+    def get_edges(self):
+        return self.__edges
+
+
 class Graph:
 
-    def __init__(self):
-        self.__graph = dict()
-        self.__vertex = []
-        self.f = []
+    def __init__(self, vertices):
+        self.__vertices = vertices
+        self.count_edge = []
+        self.init_count_edge()
 
-    def add_vertex_edge(self, vertex, edge):
-        """
-        add vertex and edge for unweighted graph
-        :param vertex: vertex graph
-        :param edge: edge graph
-        """
-        if vertex > edge:
-            if not [vertex, edge] in self.__vertex:
-                self.__vertex.append([vertex, edge])
-        else:
-            if not [edge, vertex] in self.__vertex:
-                self.__vertex.append([edge, vertex])
-        res = self.__graph.get(vertex)
-        if res is None:
-            if not edge == '':
-                self.__graph[vertex] = [edge]
-            else:
-                self.__graph[vertex] = []
-        else:
-            res.append(edge)
+    def get_vertices(self):
+        return self.__vertices
+
+    def get_index_vertex(self, vertex):
+        return self.__vertices.index(vertex)
+
+    def init_count_edge(self):
+        for vertex in self.get_vertices():
+            for edge in vertex.get_edges().keys():
+                if not (vertex.name, edge.name) in self.count_edge and not (edge.name, vertex.name) in self.count_edge:
+                    self.count_edge.append((vertex.name, edge.name))
 
     def print_graph(self):
         print("Graph")
-        for vertex, edge in self.__graph.items():
-            print('Vertex: {} edges {}'.format(vertex, edge))
+        for vertex in self.__vertices:
+            print('Vertex: {} edges: '.format(vertex))
+            for e, w in vertex.get_edges().items():
+                print('{} weight: {} '.format(e, w))
